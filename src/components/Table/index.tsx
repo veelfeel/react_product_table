a836a4b0327a839cfc105eb259c1c8924769b163
viktorfilmanovich <film.viktor@yandex.ru>
2022-12-31 20:03:40 +0300

    .....

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -23,6 +23,8 @@ export const Table: React.FC = () => {
   const productFilter = productSortSearch(products, searchValue);
   const productFilterLength = productFilter.length;
 
+  console.log(productFilter);
+
   return (
     <table className={styles.root}>
       <thead>
